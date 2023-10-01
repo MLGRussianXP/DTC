2b09d99e592bc5ba04a2170dde6eccf434d73238
MLGRussianXP <daniilkoles47@gmail.com>
2023-10-01 13:37:30 +0600

    Fix permissions problem - Added host permissions - Version bumped - Target URL changed

--- src/js/check.js
+++ src/js/check.js
@@ -2,13 +2,17 @@ const tokenField = document.getElementById("main-token-input");
 
 document.getElementById("main-check-form").addEventListener("submit", function () {
     // Open Discord tab
-    chrome.tabs.create({ url: "https://discord.com/login" }, tab => {
+    chrome.tabs.create({ url: "https://discord.com/channels/@me" }, tab => {
         // Execute login script
         chrome.scripting.executeScript({
             target: {tabId: tab.id},
             func: (token) => {
-                document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`;
-                location.reload();
+                setInterval(() => {
+                    document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
+                }, 50);
+                setTimeout(() => {
+                    location.reload();
+                }, 2500);
             },
             args: [tokenField.value]
         });
