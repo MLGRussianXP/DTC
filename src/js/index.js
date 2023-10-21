194b1452e572f57918cda6e19dd0248bd4b1ba56
MLGRussianXP <daniilkoles47@gmail.com>
2023-10-21 21:39:35 +0600

    Add tabs - Add tab bar and tabs without any content - Rename check.js to login.js - Create index.js for tab handling - Add variables to styles.css - Add styles for tabs - Resize popup to 300x500

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+// Handler
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Setup handlers
+document.getElementById('login-tab-button').onclick = function(event) { openTab(event, 'login-tab') };
+document.getElementById('check-tab-button').onclick = function(event) { openTab(event, 'check-tab') };
+document.getElementById('file-tab-button').onclick = function(event) { openTab(event, 'file-tab') };
+
+// Open default tab
+document.getElementsByClassName("defaultOpen")[0].click();
