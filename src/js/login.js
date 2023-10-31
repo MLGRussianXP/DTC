698d67ceb6b945d4919d4dc2bb3a4d5a4c8cf744
MLGRussianXP <daniilkoles47@gmail.com>
2023-10-31 14:32:43 +0600

    Move login function to the separate file

--- src/js/login.js
+++ src/js/login.js
@@ -1,22 +1,4 @@
-// Login function
-function login(login_token) {
-    // Open Discord tab
-    chrome.tabs.create({ url: "https://discord.com/channels/@me" }, tab => {
-        // Execute login script
-        chrome.scripting.executeScript({
-            target: {tabId: tab.id},
-            func: (token) => {
-                setInterval(() => {
-                    document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
-                }, 50);
-                setTimeout(() => {
-                    location.reload();
-                }, 2500);
-            },
-            args: [login_token]
-        });
-    });
-};
+import { login } from './manage-token.js';
 
 // Link to main form
 const tokenField = document.getElementById("main-token-input");
