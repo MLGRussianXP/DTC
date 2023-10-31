f8f5513fbcd4ce1480a011aa5caeca4d3ca48d1b
MLGRussianXP <daniilkoles47@gmail.com>
2023-10-31 16:40:32 +0600

    Create minimal interface on check tab - Add textarea, submit button and token list - Replace all buttons' types to "button"

--- src/js/login.js
+++ src/js/login.js
@@ -1,7 +1,22 @@
 import { login } from './manage-token.js';
 
 // Link to main form
-const tokenField = document.getElementById('main-token-input');
-document.getElementById('main-check-form').addEventListener('submit', () => {
-    login(tokenField.value)
+const loginTabTokenField = document.getElementById('main-token-input');
+document.getElementById('login-tab-button__submit').addEventListener('click', () => {
+    login(loginTabTokenField.value)
+});
+
+// Link to check form
+const checkTabTokenField = document.getElementById('check-token-input');
+const checkTabTokenList = document.getElementById('check-tab__tokens-list');
+document.getElementById('check-tab-button__submit').addEventListener('click', () => {
+    // Clear the list
+    checkTabTokenList.innerHTML = "";
+
+    // Fill the list
+    checkTabTokenField.value.split('\n').forEach(element => {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(element));
+        checkTabTokenList.appendChild(li);
+    });
 });
