c0e3f37f652030ddd48534575605b6bf206020b8
MLGRussianXP <daniilkoles47@gmail.com>
2024-01-13 23:41:04 +0600

    Remake tokens checking from files Replace icons with colored divs li now only showing the last 32 characters of the token TODO: add numeration

--- src/js/file.js
+++ src/js/file.js
@@ -5,39 +5,38 @@ const fileTabTokenList = document.getElementById('file-tab__upload-file');
 const fileTabOutputOl = document.getElementById('file-tab__output');
 
 document.getElementById('file-tab__submit').addEventListener('click', () => {
-    let file = fileTabTokenList.files[0];
+    const file = fileTabTokenList.files[0];
 
     if (file) {
         fileTabOutputOl.innerHTML = '';
 
-        let reader = new FileReader();
+        const reader = new FileReader();
 
         reader.onload = function(event) {
             let contents = event.target.result;
             let tokens = contents.split('\n');
             
             tokens.forEach(async element => {
+
                 let li = document.createElement('li');
-                li.className = 'file-tab__li'
 
-                let icon = document.createElement('img');
-                icon.className = 'file-tab__li__icon';
-                icon.width = 25;
-                icon.height = 25;
-                icon.src = '/img/icon/pending.svg';
-
-                let text = document.createElement('p');
-                text.className = 'file-tab__li__p';
-                text.innerHTML = element;
+                let icon = document.createElement('div');
+                icon.classList.add('icon-pending');
                 
+                const text = document.createElement('p');
+                text.innerHTML = "..." + element.slice(-32);
+
                 li.appendChild(icon);
                 li.appendChild(text);
                 fileTabOutputOl.appendChild(li);
+                
+                let status = await isValid(element);
+                icon.className = '';
 
-                if (await isValid(element))
-                    icon.src = '/img/icon/success.svg';
+                if (status)
+                    icon.classList.add('icon-valid');
                 else
-                    icon.src = '/img/icon/failure.svg';
+                    icon.classList.add('icon-invalid');
                 
             });
 
